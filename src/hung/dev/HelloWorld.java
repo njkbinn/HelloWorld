0deec3c1fb32f139f68b4ccccca0c2744773af49
phi hung <njkbin01@gmail.com>
2018-08-15 16:44:50 +0700

    hl

--- src/hung/dev/HelloWorld.java
+++ src/hung/dev/HelloWorld.java
@@ -3,5 +3,9 @@ package hung.dev;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld");
+		int a = 5;
+		int b = 6;
+		int tong = a + b;
+		System.out.println(Tong);
 	}
 }
