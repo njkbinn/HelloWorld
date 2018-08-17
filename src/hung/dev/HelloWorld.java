dc2342b6ef47d46c975e53ba6d9fdf45081accbd
Hoang Trong Linh <hoangtronglinh@MacBook-Air-cua-Hoang.local>
2018-08-17 15:25:28 +0700

    Giai phuong trinh bac 2

--- src/hung/dev/HelloWorld.java
+++ src/hung/dev/HelloWorld.java
@@ -2,6 +2,25 @@ package hung.dev;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("HelloWorld");
+	    int a = 10;
+	    int b = 2;
+	    int c = 1;
+	    double delta;
+	    double x1, x2;
+	    delta = (b*b) - (4*a*c);
+	    if (delta < 0) {
+	    	System.out.println("Phuong trinh vo nghiem");
+	    }
+	    
+	    if(delta > 0) {
+	    	x1 = (-b+Math.sqrt(delta))/(2*a);
+	    	x2 = (-b-Math.sqrt(delta))/(2*a);
+	    	System.out.println("x1 = " + x1);
+	    	System.out.println("x2 = " + x2);
+	    }
+	    if(delta == 0) {
+	    	x1 = -b / (2*a);
+	    	System.out.println("Phuong trinh co nghiem kep x = " + x1);
+	    }
 	}
 }
